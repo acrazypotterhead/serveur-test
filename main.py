5ac8e9ed20fb2dacfc0d791c7bc5523a8243aca2
acrazypotterhead <jvrasoamanana@gmail.com>
2025-02-12 10:47:24 +0100

    test

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ import socket
 
 import time
 from kivy.uix.textinput import TextInput
+from kivy.utils import platform
+
+if platform == 'android':
+    try:
+        from android.permissions import request_permissions, Permission
+        check_permission  # pylint: disable=import-error # type: ignore
+        request_permissions([Permission.READ_EXTERNAL_STORAGE,
+                        Permission.WRITE_EXTERNAL_STORAGE])
+    except ImportError:
+        request_permissions = None
+        Permission = None
+
 
 HEADER = 64
 PORT = 5050
@@ -14,25 +26,25 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = "192.168.56.1"
 ADDR = (SERVER, PORT)
 
-#client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#client.connect(ADDR)
-
-#def send(msg):
-#    message = msg.encode(FORMAT)
-#    msg_length = len(message)
-#    send_length = str(msg_length).encode(FORMAT)
-#    send_length += b" " * (HEADER - len(send_length))
-#    client.send(send_length)
-#    client.send(message)
-#    print(client.recv(2048).decode(FORMAT))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b" " * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
     
 class ClientApp(App):
+    
     def build(self):
         layout = GridLayout(cols=1)
         self.label = Label(text="Enter a message:")
         self.input = TextInput()
         self.button = Button(text="Send")
-        #self.button.bind(on_press=self.send_message)
+        self.button.bind(on_press=self.send_message)
         layout.add_widget(self.label)
         layout.add_widget(self.input)
         layout.add_widget(self.button)
