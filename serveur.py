08183067573bb887767baa00e743eb3f1af4a5ce
acrazypotterhead <jvrasoamanana@gmail.com>
2025-02-17 16:26:41 +0100

    oui

--- serveur.py
+++ serveur.py
@@ -4,7 +4,7 @@ import threading
 
 HEADER = 64
 PORT = 5050
-SERVER = '192.168.137.245' #socket.gethostbyname(socket.gethostname())
+SERVER = '192.168.137.1' #socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
