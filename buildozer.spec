f0e37f6fd001741c40f013e4eeb29936d2112676
acrazypotterhead <jvrasoamanana@gmail.com>
2025-02-18 17:00:58 +0100

    oui

--- buildozer.spec
+++ buildozer.spec
@@ -1,10 +1,10 @@
 [app]
 
 # (str) Title of your application
-title = My Application
+title = Serveur Test
 
 # (str) Package name
-package.name = myapp
+package.name = serv
 
 # (str) Package domain (needed for android/ios packaging)
 package.domain = org.test
--- serveur.py
+++ serveur.py
@@ -1,10 +1,13 @@
 import socket
 import threading
+import matplotlib.pyplot as plt
+
+
 
 
 HEADER = 64
 PORT = 5050
-SERVER = '192.168.137.1' #socket.gethostbyname(socket.gethostname())
+SERVER = "192.168.5.27" #socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
@@ -12,17 +15,24 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+x_arr, y_arr, z_arr = [], [], []
+
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
     connected = True
+    fig = plt.figure()
+    str = ""
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
 
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
+            str += msg
             if msg == DISCONNECT_MESSAGE:
                 connected = False
+                print(f"msg = {str}")
+                print(f"Device {addr} disconnected.")
 
             print(f"[{addr}] {msg}")
             conn.send("Message received".encode(FORMAT))
@@ -37,6 +47,8 @@ def start():
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
 
 print("[STARTING] server is starting...")
 start()
