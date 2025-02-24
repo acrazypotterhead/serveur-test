576ca6101e73488ee25228c57974418bfbe38123
acrazypotterhead <jvrasoamanana@gmail.com>
2025-02-24 17:12:43 +0100

    oui

--- serveur.py
+++ serveur.py
@@ -18,12 +18,20 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-x_axis = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+x_axis = list(range(21))
 x , y , z = [], [], []
-plt.ion
+
 fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(5, 10))
 
 class FirstWindow(Screen):
+    def __init__(self, **kwargs):
+        super(FirstWindow, self).__init__(**kwargs)
+        Clock.schedule_once(self.add_graph)
+
+    def add_graph(self, dt):
+        self.ids.box_graph.add_widget(FigureCanvasKivyAgg(fig))
+
     def update_status(self, status):
         Clock.schedule_once(lambda dt: self.ids.status_label.setter('text')(self.ids.status_label, status))
 
@@ -39,14 +47,48 @@ class FirstWindow(Screen):
                 if msg_length:
                     msg_length = int(msg_length)
                     msg = conn.recv(msg_length).decode(FORMAT)
-                    if msg == DISCONNECT_MESSAGE:
-                        connected = False
-                        self.update_status(f"Device {addr} disconnected.")
-                    self.update_messages(f"[{addr}] {msg}")
-                    conn.send("Message received".encode(FORMAT))
+
+                    split_msg = msg.split(",")
+
+                    if len(split_msg) ==3:
+                        x.append(float(split_msg[0]))
+                        y.append(float(split_msg[1]))
+                        z.append(float(split_msg[2]))
+
+                    if len(x) > 20:
+                        x.pop(0)
+                        y.pop(0)
+                        z.pop(0)
+                    
+                    ax1.clear()
+                    ax2.clear()
+                    ax3.clear()
+
+                    ax1.plot(x_axis[-len(x):], x, 'r-')
+                    ax2.plot(x_axis[-len(y):], y, 'g-')
+                    ax3.plot(x_axis[-len(z):], z, 'b-')
+
+                    ax1.set_title("X Axis")
+                    ax2.set_title("Y Axis")
+                    ax3.set_title("Z Axis")
+
+                    fig.canvas.draw()
+
+
+
+                if msg == DISCONNECT_MESSAGE:
+                    connected = False
+                    self.update_status(f"Device {addr} disconnected.")
+                self.update_messages(f"[{addr}] {msg}")
+                conn.send("Message received".encode(FORMAT))
             except:
                 connected = False
         conn.close()
+
+
+
+        
+        
 
     def start_server(self):
         server.listen()
