e4905eb9fdb8d27d54069bddf3d2f72e97a9c53d
acrazypotterhead <jvrasoamanana@gmail.com>
2025-03-06 11:19:18 +0100

    oui

--- main.py
+++ main.py
@@ -106,10 +106,9 @@ class FirstWindow(Screen):
                 print("Accelerometer is not implemented for your platform")
     
             if self.sensor:
-                #Clock.schedule_interval(self.collect_accelerometer_data, 1/90)
-                while self.sensor:
-                    self.collect_accelerometer_data()
-                    
+                Clock.schedule_interval(self.collect_accelerometer_data, 0.0001)
+                
+
                 Clock.schedule_interval(self.reset_data_count, 1)
             else:
                 accelerometer.disable()
